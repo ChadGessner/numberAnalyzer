74d719933a5025774094e1b0aca923edb667a2e6
ChadGessner <CGess83@me.com>
2022-10-01 19:44:53 -0400

    Updated to include extra credit features...

--- numberAnalyzer/Program.cs
+++ numberAnalyzer/Program.cs
@@ -8,6 +8,8 @@ namespace numberAnalyzer
         {
             while(true)
             {
+                Console.WriteLine("Do you like Apples <y> or <n>?");
+                string apples = Console.ReadLine().ToLower() == "y" ? "how do you like them apples?" : "how do you dislike them apples?"; 
                 Console.WriteLine("Enter a valid integer between 1 and 100");
                 var theNumber = Console.ReadLine();
                 if(!int.TryParse(theNumber, out var number))
@@ -23,19 +25,19 @@ namespace numberAnalyzer
                 bool isOdd = number % 2 != 0;
                 if(isOdd && number > 60)
                 {
-                    Console.WriteLine("Odd and greater than 60.");
+                    Console.WriteLine($"Odd and greater than 60. {apples}");
                 }
                 if(isOdd && number < 60)
                 {
-                    Console.WriteLine("Odd and less than 60");
+                    Console.WriteLine($"Odd and less than 60 {apples}");
                 }
                 if(!isOdd && number > 60)
                 {
-                    Console.WriteLine("Even and greater than 60.");
+                    Console.WriteLine($"Even and greater than 60. {apples}");
                 }
                 if(!isOdd && number >= 26 && number <= 60)
                 {
-                    Console.WriteLine("Even and between 26 and 60.");
+                    Console.WriteLine($"Even and between 26 and 60. {apples}");
                 }
                 if(!isOdd && number >= 2 && number <= 24)
                 {
